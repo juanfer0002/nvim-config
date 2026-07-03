b862e9299656907f799d052abf661f53f83e902f
Juan F. Muñoz M. <juanfer0002@gmail.com>
2026-07-03 18:31:50 -0500

    Add first terminal keymaps and feedline too

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,32 +5,35 @@
 
 local wk = require("which-key")
 
--- Register the group name
+--
+-- File path keymaps -  Register the group name
+--
 wk.add({
   { "<leader>fy", group = "+Yank/Copy" },
 })
 
--- Copy relative file path
 vim.keymap.set("n", "<leader>fyy", function()
   local path = vim.fn.expand("%:.")
   vim.fn.setreg("+", path)
   vim.notify("Copied relative path: " .. path)
 end, { desc = "Copy relative file path" })
 
--- Copy absolute file path
 vim.keymap.set("n", "<leader>fyY", function()
   local path = vim.fn.expand("%:p")
   vim.fn.setreg("+", path)
   vim.notify("Copied absolute path: " .. path)
 end, { desc = "Copy absolute file path" })
 
--- Copy file name
 vim.keymap.set("n", "<leader>fyn", function()
   local path = vim.fn.expand("%:t")
   vim.fn.setreg("+", path)
   vim.notify("Copied filename: " .. path)
 end, { desc = "Copy filename" })
 
+
+--
+-- Copilot
+-- 
 vim.keymap.set(
   "i",
   "<C-l>",
@@ -47,3 +50,58 @@ vim.keymap.set(
 
 vim.g.copilot_no_tab_map = true
 
+
+--
+-- Terminal 
+--
+vim.keymap.set(
+  "t",
+  "<C-h>",
+  "<C-\\><C-N><C-w>h",
+  { desc = "Terminal: Go to left window" }
+)
+
+vim.keymap.set(
+  "t",
+  "<C-j>",
+  "<C-\\><C-N><C-w>j",
+  { desc = "Terminal: Go to down window" }
+)
+
+vim.keymap.set(
+  "t",
+  "<C-k>",
+  "<C-\\><C-N><C-w>k",
+  { desc = "Terminal: Go to up window" }
+)
+
+vim.keymap.set(
+  "t",
+  "<C-l>",
+  "<C-\\><C-N><C-w>l",
+  { desc = "Terminal: Go to right window" }
+)
+
+vim.keymap.set(
+  "t",
+  "<C-q>",
+  "<C-\\><C-N>",
+  { desc = "Terminal: Exit terminal mode" }
+)
+
+--
+-- Line feed
+--
+vim.keymap.set(
+  "n",
+  "<M-o>",
+  "o<Esc>k",
+  { desc = "Insert line below" }
+)
+
+vim.keymap.set(
+  "n",
+  "<M-O>",
+  "O<Esc>j",
+  { desc = "Insert line above" }
+)
