f688dfc1c4ff806e8e81b19e033cd773d6a03319
Juan F. Muñoz <juanfer0002@hotmail.com>
2026-07-10 15:50:26 -0500

    Add snacks grep to reuse last file filter search
    When filtering by file names, like
    
    ```
    opts -- -g *.lua
    
    opts -- -g !*.lua
    ```
    
    Then, the portion after `--` will be preserved, and caret will be located at the begining
    If you need to discard everything and start from 0, simply hit: `<Esc>C`

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -8,6 +8,28 @@ return {
       opts.picker.sources[source] = vim.tbl_deep_extend("force", opts.picker.sources[source] or {}, { hidden = true })
     end
 
+    opts.picker.sources["grep"].search = function()
+      local History = require("snacks.picker.util.history")
+      local hist_store = History.new("picker_grep")
+      local records = hist_store.kv and hist_store.kv.data or {}
+
+      if #records ==0 then return "" end
+
+      local latest = records[#records]
+      local search = latest.search or ""
+
+      local regex = ".*(%-%-.*)"
+      if search:match(regex) then
+        return search:gsub(regex, "%1")
+      end
+
+      return ""
+    end
+
+    opts.picker.sources["grep"].on_show = function(picker)
+      vim.api.nvim_win_set_cursor(picker.input.win.win, { 1, 0 })
+    end
+
     opts.indent = opts.indent or {}
     opts.indent.enabled = true
     opts.indent.only_scope = false
